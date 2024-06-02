30a1d4b9a122b643c584c5872ac65a713e4879d5
hrisimov <140465371+hrisimov@users.noreply.github.com>
2024-06-02 22:20:34 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -13,6 +13,7 @@ public class Calculator {
     }
 
     // Method for multiplication
+    // Comment added from GitHub
     public double multiply(double a, double b) {
         return a * b;
     }
